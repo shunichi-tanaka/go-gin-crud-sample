58059db0087802fd3a632c92ea4799a991d2e679
田中舜一 <tanakashunichi@tanakashunichinoMacBook-Pro.local>
2022-11-21 23:21:38 +0900

    add ListUser method

--- service/user.go
+++ service/user.go
@@ -6,6 +6,7 @@ import (
 
 type UserService interface {
 	GetUser(ID string) (*models.User, error)
+	ListUser() ([]*models.User, error)
 }
 
 type userService struct {
@@ -23,4 +24,23 @@ func (us *userService) GetUser(ID string) (*models.User, error) {
 	}
 
 	return &user, nil
+}
+
+func (us *userService) ListUser() ([]*models.User, error) {
+
+	user1 := models.User{
+		ID:   ID1,
+		Name: "Bob",
+	}
+
+	user2 := models.User{
+		ID:   ID2,
+		Name: "Alice",
+	}
+	
+	ver users []*models.User
+	users = append(users, &user1)
+	users = append(users, &user2)
+
+	return users, nil
 }
